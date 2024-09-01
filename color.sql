61a30517ae39a39db0d09c2601a9c967c4c39f8e
Mwalimu-James <jameskungu10@gmail.com>
2024-09-01 07:52:30 +0300

    first commit

---
+++ color.sql
@@ -0,0 +1,14 @@
+--CREATE TABLE colors (
+    --id INTEGER PRIMARY KEY,
+   -- color TEXT NOT NULL
+    --);
+--INSERT INTO colors (id, color) VALUES (1, 'Blue');
+
+SELECT * FROM colors
+
+--CREATE TABLE emp(empid INTEGER NOT NULL PRIMARY KEY, empname TEXT NOT NULL, 
+--email NOT NUULL);
+
+--INSERT INTO emp(empname,email)
+--VALUES("Sam","abc@gmail.com");
+SELECT * FROM emp;
---
+++ details.sql
@@ -0,0 +1,16 @@
+--CREATE TABLE students(studentsid INTEGER NOT NULL PRIMARY KEY, 
+--studentsname TEXT NOT NULL, email NOT NULL, 
+--);
+--ALTER TABLE students ADD COLUMN studentsusername TEXT NOT NULL;
+--UPDATE students SET studentsusername = 'sam@001';
+--ALTER TABLE students ADD COLUMN studentspassword ;
+--UPDATE students SET studentspassword = 'samsecretkey@001';
+--UPDATE students SET studentsusername = 'mike@001' WHERE student_id = 2;
+--UPDATE students SET studentspassword = 'mikesecretkey@001' WHERE student_id = 2;
+
+--INSERT INTO students(studentsid, studentsname, email, studentsusername)
+VALUES (1,"Sam","Sam@.com","sam@001"
+    2,"Mike","Mike@.com","mike@001");
+
+
+--SELECT * FROM students
